8e3233b7c9ac43d41b0116d9ee341d1fb5ca9fad
ilker <ilkerdemir@yandex.com.tr>
2017-07-27 20:15:25 +0000

    Blog wf commiti

---
+++ bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/usr/local/bin/python3.6
+# EASY-INSTALL-SCRIPT: 'Django==1.11.3','django-admin.py'
+__requires__ = 'Django==1.11.3'
+__import__('pkg_resources').run_script('Django==1.11.3', 'django-admin.py')
